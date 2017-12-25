14ab53b03b7fc8399fefd2dfb035d5f7f8060e70
Dana Kirsh <kirsh.dana@gmail.com>
2017-12-25 12:49:13 +0000

    Separated sequitur and sequitur format to create tests Digram uniquness also in existing rules Added tests

--- sequitur.js
+++ sequitur.js
@@ -1,18 +1,15 @@
+/* Handler for change of string. */
 $(document).ready(function() {
   $("#input").on("change paste keyup", function() {
      $("#input").val($("#input").val().toLowerCase());
-    $("#result").text(sequitur($("#input").val()));
+    $("#result").text(sequiturFormat($("#input").val()));
   });
 });
 
-function sequitur(str) {
-  var s = [str.charAt(0)];
-  var dict = {'currentRule': 'A', rules: {}};
-  for (var i = 1; i < str.length; i++) {
-    s.push(str.charAt(i));
-    s = compress(s, dict)
-  }
-  str = "S\u2192" + s.join("");
+/* Formats sequitur output. */
+function sequiturFormat(str) {
+  var dict = sequitur(str);
+  str = "S\u2192" + dict["s"];
   delete dict['currentRule'];
   Object.keys(dict.rules).forEach(function(key, index) {
     str = str + "\n" + dict.rules[key].name + "\u2192" + key + " (" + dict.rules[key].ocurrences +")";
@@ -20,8 +17,21 @@ function sequitur(str) {
   return str;
 }
 
+/* Performs sequitur. */
+function sequitur(str) {
+  var dict = {'currentRule': 'A', rules: {}};
+  var s = [str.charAt(0)];
+  for (var i = 1; i < str.length; i++) {
+    s.push(str.charAt(i));
+    s = compress(s, dict);
+  }
+  dict["s"] = s.join("");
+  return dict;
+}
+
+/* Main compression loop. */
 function compress(s, dict) {
-    var digrams = {};
+    var digrams = getRulesDigrams(dict);
     for (var i = 0; i < s.length - 1; i++) {
       var digram = s[i] + s[i + 1];
       if (dict.rules.hasOwnProperty(digram)) {
@@ -34,11 +44,16 @@ function compress(s, dict) {
         }
       }
       else if (digrams.hasOwnProperty(digram) && digrams[digram] != i - 1) {
-        var currentRule = dict['currentRule']
-        dict.rules[digram] = {'name': currentRule, 'ocurrences' : 2};
+        var currentRule = dict['currentRule'];
         replace2Elements(s, i, currentRule);
-        replace2Elements(s, digrams[digram], currentRule);
+        if ((typeof digrams[digram]) == 'number') {
+          replace2Elements(s, digrams[digram], currentRule);          
+        }
+        else {
+          fixCurrentRules(dict, digram);
+        }
         delete digrams[digram];
+        dict.rules[digram] = {'name': currentRule, 'ocurrences' : 2};
         dict['currentRule'] = inrementRule(currentRule);
         updateOcurrencesDigram(dict, digram);
       }
@@ -46,28 +61,57 @@ function compress(s, dict) {
          digrams[digram] = i;
       }
     }
-  return s;
+	return s;
 }
 
+/* Gets all digrams in existing rules. */
+function getRulesDigrams(dict) {
+  var digrams = {};
+  Object.keys(dict.rules).forEach(function(key, index) {
+    for (var i = 0; i < key.length - 1; i++) {
+      var digram = key.charAt(i) + key.charAt(i + 1);
+      digrams[digram] = dict.rules[key].name; 
+    }
+  });
+  return digrams;
+}
+
+/* Changes existing rules to use new rule. */
+function fixCurrentRules(dict, digram) {
+	Object.keys(dict.rules).forEach(function(oldRule) {
+		if (oldRule.includes(digram)) {
+		  var newRule = oldRule.replace(digram, dict.currentRule);
+		  changeRule(dict, oldRule, newRule);
+		}
+	});
+}
+
+/* Changes an existing rule. */
+function changeRule(dict, oldName, newName) {
+  var newRule = dict.rules[oldName];
+  dict.rules[newName] = newRule;
+  delete dict.rules[oldName];
+}
+
+/*  */
 function updateOcurrencesDigram(dict, digram) {
   updateOcurrencesOneChar(dict, 0, digram);
   updateOcurrencesOneChar(dict, 1, digram);  
 }
 
+/*  */
 function updateOcurrencesOneChar(dict, i, digram) {
   var char = digram.charAt(i);
   if (char.match(new RegExp("[A-Z]"))) {
     Object.keys(dict.rules).forEach(function(key, index) {
-		var rule = dict.rules[key]
-		  if (rule && rule.name == char) {
-        rule.ocurrences--;
-        if (rule.ocurrences == 1) {
-          var newRule = dict.rules[digram];
-          dict.rules[digram.replace(char, key)] = newRule;
-          delete dict.rules[digram];
-          delete dict.rules[key];
-        }
-		  }
+		var rule = dict.rules[key];
+		if (rule && rule.name == char) {
+			rule.ocurrences--;
+			if (rule.ocurrences == 1) {
+				changeRule(dict, digram, digram.replace(char, key));
+				delete dict.rules[key];
+			}
+		}
     });
   }  
 }
--- tests.js
+++ tests.js
@@ -1,8 +1,97 @@
 QUnit.test( "Increment rule A > B", function(assert) {
   assert.equal(inrementRule("A"), "B");
 });
+
 QUnit.test( "Merge letters in array", function(assert) {
 	var arr = ["a", "b"];
 	replace2Elements(arr, 0, "A");
 	assert.equal(arr[0], "A");
+	assert.equal(arr[0], "A");
+});
+
+QUnit.test( "abab - simple rule.", function(assert) {
+	var dict = sequitur("abab");
+	var expectedDict = {
+		"currentRule": "B",
+		"rules": {
+			"ab": {
+				"name": "A",
+				"ocurrences": 2
+			}
+		},
+	  "s": "AA"
+	};
+	assert.deepEqual(dict, expectedDict);
+});
+
+QUnit.test( "abcdbcabcdbc.", function(assert) {
+	var dict = sequitur("abcdbcabcdbc");
+	var expectedDict = {
+		"currentRule": "E",
+		"rules": {
+			"bc": {
+				"name": "A",
+				"ocurrences": 2
+			},
+			"aAdA": {
+				"name": "D",
+				"ocurrences": 2
+			}
+		},
+		"s": "DD"
+	};
+	assert.deepEqual(dict, expectedDict);
+});
+
+QUnit.test( "abcdbcabcd.", function(assert) {
+	var dict = sequitur("abcdbcabcd");
+	var expectedDict = {
+		"currentRule": "D",
+		"rules": {
+			"bc": {
+				"name": "A",
+				"ocurrences": 2
+			},
+			"aAd": {
+				"name": "C",
+				"ocurrences": 2
+			}
+		},
+		"s": "CAC"
+	};
+	assert.deepEqual(dict, expectedDict);
+});
+
+QUnit.test( "aabaaab - same letter repeating 3 times.", function(assert) {
+	var dict = sequitur("aabaaab");
+	var expectedDict = {
+		"currentRule": "B",
+		"rules": {
+			"aa": {
+				"name": "A",
+				"ocurrences": 2
+			}
+		},
+	  "s": "AbAab"
+	};
+	assert.deepEqual(dict, expectedDict);
+});
+
+QUnit.test( "abcabcab - same digram in s and in one of the rules, creates a rule that changes an existing rule.", function(assert) {
+	var dict = sequitur("abcabcab");
+	var expectedDict = {
+		"currentRule": "D",
+		"rules": {
+			"Cc": {
+				"name": "B",
+				"ocurrences": 2
+			},
+			"ab": {
+				"name": "C",
+				"ocurrences": 2
+			}
+		},
+	  "s": "BBC"
+	};
+	assert.deepEqual(dict, expectedDict);
 });
